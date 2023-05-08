0815ba8c914f1372285d040c441566f05d26d4a6
AtharvaKarkar <93367238+AtharvaKarkar@users.noreply.github.com>
2023-05-08 10:21:38 +0530

    project complete

--- script.js
+++ script.js
@@ -1,20 +1,94 @@
-const section4 = document.querySelector('.section-4');
-const section4Left1 = document.querySelector('.section-4-left-1');
-const section4Left2 = document.querySelector('.section-4-left-2');
-const section4Left3 = document.querySelector('.section-4-left-4');
+const section4 = document.querySelector(".section-4");
 
-const section4RightImg = document.querySelector('.section-4-right-image');
+const section4Left1 = document.querySelector(".section-4-left-1");
+const section4Left2 = document.querySelector(".section-4-left-2");
+const section4Left3 = document.querySelector(".section-4-left-3");
+const section4Left4 = document.querySelector(".section-4-left-4");
 
-const section4RightImg1 = "https://web-images.credcdn.in/_next/assets/images/home-page/features/neopop-fold1.png";
-const section4RightImg2 = "https://web-images.credcdn.in/_next/assets/images/home-page/features/neopop-fold2.png";
-const section4RightImg3 = "https://web-images.credcdn.in/_next/assets/images/home-page/features/neopop-fold3.png";
-const section4RightImg4 = "https://web-images.credcdn.in/_next/assets/images/home-page/features/neopop-fold4.png";
+const section4RightImg = document.querySelector(".section-4-right-img");
 
-//adding a scroll event
-window.addEventListener('scroll', () => {
-    const scrollPosition = window.scrollY // stores the current vertical scroll position on the window in pixels
-    const section4OffsetTop = section4.section4OffsetTop  // stores the distance in pixels between the top of section4 is assumed to be reference to a DOM element on the page. 'offset' is a property of the element object that returns the distance in pixels between the top of the element and the top of its offsetParent element
-    const section4Height = section4.clientHeight  // stores the height of the section4 element in pixels. clientHeight is a property of the element object that returns the inner height of an element, excluding its padding, border, and margin.
+const section4RightImg1 =
+  "https://web-images.credcdn.in/_next/assets/images/home-page/features/neopop-fold1.png";
+const section4RightImg2 =
+  "https://web-images.credcdn.in/_next/assets/images/home-page/features/neopop-fold2.png";
+const section4RightImg3 =
+  "https://web-images.credcdn.in/_next/assets/images/home-page/features/neopop-fold3.png";
+const section4RightImg4 =
+  "https://web-images.credcdn.in/_next/assets/images/home-page/features/neopop-fold4.png";
 
-    if()
-})
+// add scroll event listener
+
+window.addEventListener("scroll", () => {
+    
+  const scrollPosition = window.scrollY; //will get the current scroll position of the window and assigns it to the 'scrollPosition' constant.
+
+  const section4OffsetTop = section4.offsetTop;
+
+  const section4Height = section4.clientHeight;
+
+  // checking if the scrollPostion is within the range of section4 elements top offset and height.
+
+  if (
+    scrollPosition > section4OffsetTop &&
+    scrollPosition < section4Height + section4OffsetTop
+  ) {
+    if (
+      scrollPosition > section4OffsetTop &&
+      scrollPosition < section4OffsetTop + 300
+    ) {
+      section4Left1.style.opacity = 1;
+      section4Left1.style.transition = "opacity 1s ease-in-out";
+    } else if (
+      scrollPosition > section4OffsetTop + innerHeight &&
+      scrollPosition < section4OffsetTop + innerHeight + 300
+    ) {
+      section4Left2.style.opacity = 1;
+      section4Left2.style.transition = "opacity 1s ease-in-out";
+    } else if (
+      scrollPosition > section4OffsetTop + innerHeight * 2 &&
+      scrollPosition < section4OffsetTop + innerHeight * 2 + 300
+    ) {
+      section4Left3.style.opacity = 1;
+      section4Left3.style.transition = "opacity 1s ease-in-out";
+    } else if (
+      scrollPosition > section4OffsetTop + innerHeight * 3 &&
+      scrollPosition < section4OffsetTop + innerHeight * 3 + 300
+    ) {
+      section4Left4.style.opacity = 1;
+      section4Left4.style.transition = "opacity 1s ease-in-out";
+    } else {
+      section4Left1.style.opacity = 0;
+      section4Left2.style.opacity = 0;
+      section4Left3.style.opacity = 0;
+      section4Left4.style.opacity = 0;
+    }
+
+    if (
+      scrollPosition > section4OffsetTop &&
+      scrollPosition < section4OffsetTop + innerHeight
+    ) {
+      section4RightImg.src = section4RightImg1;
+      section4RightImg.style.transition = "opacity 1s ease-in-out";
+    } else if (
+      scrollPosition > section4OffsetTop + innerHeight &&
+      scrollPosition < section4OffsetTop + innerHeight * 2
+    ) {
+      section4RightImg.src = section4RightImg2;
+      section4RightImg.style.transition = "opacity 1s ease-in-out";
+    } else if (
+      scrollPosition > section4OffsetTop + innerHeight * 2 &&
+      scrollPosition < section4OffsetTop + innerHeight * 3
+    ) {
+      section4RightImg.src = section4RightImg3;
+      section4RightImg.style.transition = "opacity 1s ease-in-out";
+    } else if (
+      scrollPosition > section4OffsetTop + innerHeight * 3 &&
+      scrollPosition < section4OffsetTop + innerHeight * 4
+    ) {
+      section4RightImg.src = section4RightImg4;
+      section4RightImg.style.transition = "opacity 1s ease-in-out";
+    } else {
+      section4RightImg.src = section4RightImg1;
+    }
+  }
+});

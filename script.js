648b6f9d801d0f12a130a404326a14e01dbfe089
AtharvaKarkar <93367238+AtharvaKarkar@users.noreply.github.com>
2023-05-08 09:26:44 +0530

    changes

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 const section4 = document.querySelector('.section-4');
+const section4Left1 = document.querySelector('.section-4-left-1');
+const section4Left2 = document.querySelector('.section-4-left-2');
+const section4Left3 = document.querySelector('.section-4-left-4');
 
+const section4RightImg = document.querySelector('.section-4-right-image');
+
+const section4RightImg1 = "https://web-images.credcdn.in/_next/assets/images/home-page/features/neopop-fold1.png";
+const section4RightImg2 = "https://web-images.credcdn.in/_next/assets/images/home-page/features/neopop-fold2.png";
+const section4RightImg3 = "https://web-images.credcdn.in/_next/assets/images/home-page/features/neopop-fold3.png";
+const section4RightImg4 = "https://web-images.credcdn.in/_next/assets/images/home-page/features/neopop-fold4.png";
+
+//adding a scroll event
+window.addEventListener('scroll', () => {
+    const scrollPosition = window.scrollY // stores the current vertical scroll position on the window in pixels
+    const section4OffsetTop = section4.section4OffsetTop  // stores the distance in pixels between the top of section4 is assumed to be reference to a DOM element on the page. 'offset' is a property of the element object that returns the distance in pixels between the top of the element and the top of its offsetParent element
+    const section4Height = section4.clientHeight  // stores the height of the section4 element in pixels. clientHeight is a property of the element object that returns the inner height of an element, excluding its padding, border, and margin.
+
+    if()
+})
